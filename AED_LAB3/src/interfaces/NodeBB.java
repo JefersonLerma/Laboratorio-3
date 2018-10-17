dd5249b14c67475854a7dd80483f0e19bcf55745
jeffe <jeffe@172.30.184.160>
2018-10-17 08:51:15 -0500

    I completed the methods of the tree interface, and started the interface TreeBB

--- AED_LAB3/src/interfaces/NodeBB.java
+++ AED_LAB3/src/interfaces/NodeBB.java
@@ -1,5 +1,10 @@
 package interfaces;
 
 public interface NodeBB<K,T> {
+	
+	public static final char LEFT='l';
+	public static final char RIGHT='r';
+	public char getRelativePocision();
+	public void setRelativePocision(char pos);
 
 }
--- AED_LAB3/src/interfaces/Tree.java
+++ AED_LAB3/src/interfaces/Tree.java
@@ -1,8 +1,20 @@
 package interfaces;
 
+import java.util.Collection;
+
 public interface Tree<K,T> {
 	
 	public void add(K key, T element);
 	public void add(NodeBB<K, T> toAdd);
-
+	public int size();
+	public int height();
+	public void delete(K key);
+	public void delete(NodeBB<K, T> key);
+	public boolean exist(K elemento);
+	public void inOrden(Collection<T> colection);
+	public void posOrden(Collection<T> colection);
+	public void preOrden(Collection<T> colection);
+	public T search(K key);
+	public NodeBB<K,T> getRaiz();
+	public void setRaiz(NodeBB<K,T> nu);
 }
