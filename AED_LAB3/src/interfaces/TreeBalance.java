3dabd248ccc7606e02cf2630b801c9777c2d1175
jeffe <jeffe@172.30.184.160>
2018-10-17 09:41:11 -0500

    I made the methods of the class TreeBanlance

--- AED_LAB3/src/interfaces/TreeBalance.java
+++ AED_LAB3/src/interfaces/TreeBalance.java
@@ -1,5 +1,10 @@
 package interfaces;
 
-public interface TreeBalance {
+public interface TreeBalance<K extends Comparable<K>,T> extends Tree<K, T>{
+	
+    public void fixAfterDeletion(NodeBB<K, T> x);
+    public void fixAfterInsertion(NodeBB<K, T> n);
+    public void rotateLeft(NodeBB<K, T> p);
+    public void rotateRight(NodeBB<K, T> p);
 
 }
