b0f40cfde5a4c1b601d37193687912d052f4b268
danielGomez0 <madafakam60@gmail.com>
2018-10-18 23:32:51 -0500

    I added atributes to NodeAVL

--- AED_LAB3/src/interfaces/NodeAvl.java
+++ AED_LAB3/src/interfaces/NodeAvl.java
@@ -2,4 +2,12 @@ package interfaces;
 
 public interface NodeAvl<K extends Comparable<K>,T> extends NodeBB<K, T>{
 
+	public static final int WEIGHT_LEFT=1;
+	public static final int WEIGHT_RIGHT=-1;
+	public static final int BALANCED=0;
+	public int getFactor();
+	public void setFactor(int fa);
+	public void asignFactors();
+
+	
 }
