2985afd632edc83382efe6d1729bb7fefbc1fd99
jeffe <jeffe@DESKTOP-SOUHA69>
2018-10-23 16:45:36 -0500

    I implemented and completed the nodeAvl class.

--- AED_LAB3/src/wolrd/NodeAvl.java
+++ AED_LAB3/src/wolrd/NodeAvl.java
@@ -1,6 +1,45 @@
 package wolrd;
 import interfaces.*;
 
-public class NodeAvl{
 
+public class NodeAvl<K extends Comparable<K>,T> extends NodeBB<K, T> implements NodeAvlJD<K, T>{
+	
+	private int factor;
+	public NodeAvl( K key,T element) {
+		super(key, element);
+		setFactor(BALANCED);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getFactor() {
+		// TODO Auto-generated method stub
+		return factor;
+
+	}
+	
+	@Override
+	public void setFactor(int fa) {
+		// TODO Auto-generated method stub
+		factor=fa;
+	}
+	
+	
+	public void asignFactors(){
+		int aI=0;
+		int aD=0;
+		if(getRight()!=null){
+			aD=getRight().height();
+			((NodeAvlJD<K, T>) getRight()).asignFactors();
+			
+		}
+		if(getLeft()!=null){
+			aI=getLeft().height();
+			((NodeAvlJD<K, T>) getLeft()).asignFactors();
+
+		}
+		factor=aI-aD;
+	}
+
+	
 }
