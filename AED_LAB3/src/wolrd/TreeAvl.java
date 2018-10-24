323d4ed96ef047196dae23cc6afbfe8c8c78cf2b
jeffe <jeffe@172.30.162.174>
2018-10-24 07:53:00 -0500

    creation of the add method of the TreeAvl class

--- AED_LAB3/src/wolrd/TreeAvl.java
+++ AED_LAB3/src/wolrd/TreeAvl.java
@@ -1,5 +1,18 @@
 package wolrd;
 
-public class TreeAvl {
+import interfaces.*;
 
+public class TreeAvl<K extends Comparable<K>,T> extends TreeBR<K, T> implements TreeBalancedJD<K, T> {
+
+	@Override
+	public void add(K key, T element) {
+		NodeAvlJD<K, T> toAdd=new NodeAvl<K, T>(key, element);
+		super.add(toAdd);
+		int s=size();
+		fixAfterInsertion(toAdd);
+		getRaiz().setSize(s);
+	}
+	
+	
+	
 }
