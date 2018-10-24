9d605f409fcdc290ff4e97ec0ff5edce9ca7eac6
danielGomez0 <madafakam60@gmail.com>
2018-10-23 21:08:42 -0500

    I finished the NodeBR class.

--- AED_LAB3/src/wolrd/NodeBR.java
+++ AED_LAB3/src/wolrd/NodeBR.java
@@ -1,8 +1,51 @@
 package wolrd;
 
-public class NodeBR {
+import interfaces.*;
+import javafx.scene.Node;
+
+
+public class NodeBR<K extends Comparable<K>,T> extends NodeBB<K, T> implements NodeBRJD<K, T> {
 
 	
-	private int pointer = 0;
+	
+	private char color;
+	public NodeBR( K key,T element) {
+		super(key, element);
+	
+		setColor(COLOR_RED);
+		
+	}
+	public char getColor() {
+		return color;
+	}
+	public void setColor(char color) {
+		this.color = color;
+	}
+	private int verificarI(){
+		int ret=0;
+       if(isLeaf()){
+    	   
+			return color==NodeBRJD.COLOR_BLACK?2:1;
+		}
+       else if(getRight()!=null&&getLeft()!=null){
+    	   
+       }else{
+    	   
+       }
+		
+		return ret;
+	}
+	public boolean verificar(){
+		boolean ret=false;
+		if(isLeaf()){
+			verificarI();
+		}else{
+			
+		}
+		
+		return ret;
+				
+				
+	}
 
 }
