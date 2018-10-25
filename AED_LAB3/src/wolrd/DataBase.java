b77c094769ec5877dd00b189d6c380375a599a51
jeffe <jeffe@DESKTOP-SOUHA69>
2018-10-24 23:27:01 -0500

    add the database of the players, and create the main class of the program, also create the constants that represent the criteria to evaluate for the players.

---
+++ AED_LAB3/src/wolrd/DataBase.java
@@ -0,0 +1,15 @@
+package wolrd;
+
+public class DataBase {
+	
+    //data path
+	public final static String FILE_PATH = "./data/Players/NBA_DATA.csv";
+	// Selected statistical criteria
+	public final static String TRUE_SHOOTING_PERCENTAGE = "TS%";
+	public final static String OFFENSIVE_REBOUND_PERCENTAGE = "ORB%";
+	public final static String DEFENSIVE_REBOUND_PERCENTAGE = "DRB%";
+	public final static String TOTAL_REBOUND_PERCENTAGE = "TRB%";
+	public final static String ASSIST_PERCENTAGE = "ATS%";
+	
+	
+}
