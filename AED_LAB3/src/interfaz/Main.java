f804157e9e84bd91727865af09df7f61eb4bb6d9
jeffe <jeffe@DESKTOP-SOUHA69>
2018-10-25 00:19:23 -0500

    creating method that loads file, and creating some tests to ensure correct reading.

--- AED_LAB3/src/interfaz/Main.java
+++ AED_LAB3/src/interfaz/Main.java
@@ -1,19 +1,29 @@
 package interfaz;
 
 import java.awt.BorderLayout;
+import java.io.IOException;
+
+import wolrd.*;
 
 import javax.swing.JFrame;
 
 public class Main extends JFrame{
 	
-	
+	private DataBase xd;
 	public Main() {
 		// TODO Auto-generated constructor stub
 		setTitle("Players");
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
+		xd = new DataBase();
 		
+		try {
+			xd.fileUpload();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
--- AED_LAB3/src/wolrd/DataBase.java
+++ AED_LAB3/src/wolrd/DataBase.java
@@ -1,10 +1,15 @@
 package wolrd;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import interfaces.*;
 
 public class DataBase {
 	
     //data path
-	public final static String FILE_PATH = "./data/Players/NBA_DATA.csv";
+	public final static String FILE_PATH = "./data/Players/x.txt";
 	// Selected statistical criteria
 	public final static String TRUE_SHOOTING_PERCENTAGE = "TS%";
 	public final static String OFFENSIVE_REBOUND_PERCENTAGE = "ORB%";
@@ -12,7 +17,40 @@ public class DataBase {
 	public final static String TOTAL_REBOUND_PERCENTAGE = "TRB%";
 	public final static String ASSIST_PERCENTAGE = "ATS%";
 	
+	private TreeBR<Integer, Player> playersBR;
+	private TreeAvl<Integer, Player> playersAVL;
 	
 	
 	
+	
+	
+	public void fileUpload() throws IOException{
+		
+		playersAVL = new TreeAvl<>();
+		playersBR = new TreeBR<>();
+		File file = new File(FILE_PATH);
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		String a = "";
+		
+		while (((a = br.readLine()) != null) && a.trim().length() != 0)  {
+			if (!a.startsWith("Year")) {
+				String [] data  = a.split(",");
+				System.out.println(data[2]);
+				System.out.println(data[3]);
+				System.out.println(data[1]);
+				System.out.println(data[7]);
+				System.out.println(data[9]);
+				System.out.println(data[10]);
+				System.out.println(data[12]);
+				System.out.println(data[13]);
+				
+				
+				
+			}
+			
+			
+		}
+		
+	}
 }
